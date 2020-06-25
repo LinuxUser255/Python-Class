33dfeac392de1a16016354f94eef9598df0be212
Chris <atnl@protonmail.com>
2020-06-25 16:21:26 +0000

    Add files via upload

---
+++ opsandflow.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+#Operators
+# = assignment
+# + addition
+# - subtraction
+# * multiplication
+# / division (float results)
+# // truncation division
+# ** exponets
+# % modulus
+
+
+print(5+7)
+print(9*43)
+
+print(8**12)
+# 8^12
+
+# Control Flow
+# < less than
+# <= less than or equal
+# == equal
+# > greater than
+# >= greaater thaan or equal 
+# != Not Equal 
+# is/is not, Object identify/ negate
+# in/ not in,  is inside/negate
+# And, Lgical AND
+# OR, Logical OR
+# NOT, Logical NOT
+
+
